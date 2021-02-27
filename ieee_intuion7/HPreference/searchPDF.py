13845964d5c1583943af6bdc192472d017351d67
Tangolin <65639845+Tangolin@users.noreply.github.com>
2021-02-28 02:11:19 +0800

    Update searchPDF.py

--- ieee_intuion7/HPreference/searchPDF.py
+++ ieee_intuion7/HPreference/searchPDF.py
@@ -3,8 +3,11 @@ import numpy as np
 import pandas as pd
 from pandas.plotting import table 
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.io
 
 def find(query):
+
     mylist = []
 
     pdf = pdfplumber.open('HP.pdf')
@@ -16,17 +19,25 @@ def find(query):
                 for j in range(cur.shape[0]):
                     if(query in cur[j][0]):
                         mylist.append(cur[j])
+
             except:
                 pass
 
-    for i in range(len(mylist)):
-        for j in range(len(mylist[i])):
-            mylist[i][j] = mylist[i][j].replace('\n','')
-
-    mypanda = pd.DataFrame(mylist)
-    ax = plt.subplot(111, frame_on=False)
-    ax.xaxis.set_visible(False)
-    ax.yaxis.set_visible(False)
-    table(ax, mypanda)
-
-    plt.savefig('Requirements.png')
+    if(len(mylist) != 0):
+        for i in range(len(mylist)):
+            for j in range(len(mylist[i])):
+                mylist[i][j] = mylist[i][j].replace('\n','')
+    
+        mypanda = pd.DataFrame(mylist)
+        fig = go.Figure(data=[go.Table(header=dict(values=list(mypanda.columns),
+                        fill_color='paleturquoise',
+                        align='left'),
+                        cells=dict(values=[mypanda[item] for item in mypanda.columns],
+                        fill_color='lavender',
+                        align='left'))])
+        fig.show()
+        # plotly.io.write_image(fig,'requirements.png',format='png',width=1500,height=1500)
+        return True 
+        
+    else:
+        return False
