eb3f49780d55e50819f98ddd6a0d1e78a2ed2de8
Chicken_Fanny <59469707+1234GoodDawg@users.noreply.github.com>
2021-02-28 05:33:08 +0800

    Update translator.py

--- ieee_intuion7/HPreference/translator.py
+++ ieee_intuion7/HPreference/translator.py
@@ -4,9 +4,12 @@ import tabula
 import pandas as pd
 import plotly.figure_factory as ff
 import os
+from docx import Document
 
 file = os.listdir('/temp/')
 
+def astype_per_column(df: pd.DataFrame, column: str, dtype):
+    df[column] = df[column].astype(dtype)
 
 if file.endswith('.pdf'):
 
@@ -15,9 +18,6 @@ if file.endswith('.pdf'):
 
     translator = google_translator()
     
-    def astype_per_column(df: pd.DataFrame, column: str, dtype):
-        df[column] = df[column].astype(dtype)
-  
     astype_per_column(df, df.columns[1], str)
   
     for i in range(len(df)):
@@ -25,19 +25,43 @@ if file.endswith('.pdf'):
   
     df.to_csv('translated_data.csv',sep=',')
     
+    
 else if file.endswith('.xlsx'):
+    
     
     df2 = pd.read_excel(file)
     df2 = df2.take([3,25],axis=1)
-  
+    
     indexes_to_drop = list(range(0,25)) + list(range(38,50))
     indexes_to_keep = set(range(df2.shape[0])) - set(indexes_to_drop)
     df_sliced = df2.take(list(indexes_to_keep))
-
+    
     astype_per_column(df_sliced, df_sliced.columns, str)
+    
     df_sliced.reset_index(inplace=True)
-
+    
     for i in range(len(df_sliced)):
         df_sliced.loc[i,'Name'] = translator.translate(df_sliced.iloc[i,1], lang_tgt = 'en')
     
     df_sliced.to_csv('translated_data.csv',sep=',')
+    
+    
+else if file.endswith('.docx'):
+    
+    document = Document(file)
+    table = document.tables[0]
+    data = [[cell.text for cell in row.cells] for row in table.rows]
+    
+    df = pd.DataFrame(data)
+    df.rename(columns=df.iloc[0], inplace=True)
+    df.drop(df.index[0], inplace=True)
+    
+    translator = google_translator()
+    
+    astype_per_column(df, df.columns[1], str)
+    
+    for i in range(len(df)):
+        df.loc[i,'Name'] = translator.translate(df.iloc[i,1], lang_tgt = 'en')
+    
+    
+    df.to_csv('translated_data.csv',sep=',')
