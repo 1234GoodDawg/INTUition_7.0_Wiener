3cec2568ceaf9f3318840c313959cce03eed4ad3
TruSloth <foo.lucas@yahoo.com.sg>
2021-02-27 10:22:12 +0800

    Delete demo test

--- ieee_intuion7/lib/main.dart
+++ ieee_intuion7/lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(WebApp());
+}
+
+class WebApp extends StatefulWidget {
+  @override
+  _WebAppState createState() => _WebAppState();
+}
+
+class _WebAppState extends State<WebApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
 }
