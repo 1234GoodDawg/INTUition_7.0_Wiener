0962a7902faa031dd305b76511944eef8cf79c8e
Tangolin <65639845+Tangolin@users.noreply.github.com>
2021-02-27 21:05:08 +0800

    Update searchPDF.py

--- ieee_intuion7/HPreference/searchPDF.py
+++ ieee_intuion7/HPreference/searchPDF.py
@@ -4,29 +4,29 @@ import pandas as pd
 from pandas.plotting import table 
 import matplotlib.pyplot as plt
 
-query = ''
-mylist = []
+def find(query):
+    mylist = []
 
-pdf = pdfplumber.open('HP.pdf')
+    pdf = pdfplumber.open('HP.pdf')
 
-for i in range(len(pdf.pages)):
-    if(query in pdf.pages[i].extract_text()):
-        try:
-            cur = np.array(pdf.pages[i].extract_table())
-            for j in range(cur.shape[0]):
-                if(query in cur[j][0]):
-                    mylist.append(cur[j])
-        except:
-            pass
+    for i in range(len(pdf.pages)):
+        if(query in pdf.pages[i].extract_text()):
+            try:
+                cur = np.array(pdf.pages[i].extract_table())
+                for j in range(cur.shape[0]):
+                    if(query in cur[j][0]):
+                        mylist.append(cur[j])
+            except:
+                pass
 
-for i in range(len(mylist)):
-    for j in range(len(mylist[i])):
-        mylist[i][j] = mylist[i][j].replace('\n','')
+    for i in range(len(mylist)):
+        for j in range(len(mylist[i])):
+            mylist[i][j] = mylist[i][j].replace('\n','')
 
-mypanda = pd.DataFrame(mylist)
-ax = plt.subplot(111, frame_on=False)
-ax.xaxis.set_visible(False)
-ax.yaxis.set_visible(False)
-table(ax, mypanda)
+    mypanda = pd.DataFrame(mylist)
+    ax = plt.subplot(111, frame_on=False)
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
+    table(ax, mypanda)
 
-plt.savefig('Requirements.png')
+    plt.savefig('Requirements.png')
