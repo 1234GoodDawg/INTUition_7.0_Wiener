4b62eeea7a7d1646a90158ff11b3a1107a690ab3
Lucas Foo <39505341+TruSloth@users.noreply.github.com>
2021-02-28 08:32:45 +0800

    Update searchPDF.py

--- ieee_intuion7/HPreference/searchPDF.py
+++ ieee_intuion7/HPreference/searchPDF.py
@@ -12,6 +12,7 @@ def find(query, num):
     mylist = []
 
     pdf = pdfplumber.open('HP.pdf')
+    os.mkdir('/tmp/requirements/')
 
     for i in range(len(pdf.pages)):
         if(query in pdf.pages[i].extract_text()):
@@ -40,7 +41,7 @@ def find(query, num):
                         fill_color='lavender',
                         align='left'))])
 
-        plotly.io.write_image(fig,'/tmp/requirements_'+f'{i}'+'.png',format='png',width=1500,height=1500)
+        plotly.io.write_image(fig,'/tmp/requirements/requirements_'+f'{i}'+'.png',format='png',width=1500,height=1500)
         return True 
         
     else:
