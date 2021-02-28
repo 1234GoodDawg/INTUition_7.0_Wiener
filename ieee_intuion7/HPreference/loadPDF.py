e123394677e76db11a2edd3c9ff3e7e0760ce735
Tangolin <65639845+Tangolin@users.noreply.github.com>
2021-02-28 08:42:41 +0800

    Update loadPDF.py

--- ieee_intuion7/HPreference/loadPDF.py
+++ ieee_intuion7/HPreference/loadPDF.py
@@ -4,10 +4,14 @@ import urllib as ul
 import pdfplumber
 import numpy as np
 
+mypath = '/tmp/'
+if not os.path.isdir(mypath):
+    os.makedirs(mypath)
+        
 gen = search('HP Standard 011 General Specification for the Environment', tld='com', lang='en', num=10, start=0, stop=None, pause=2.0)
 url = next(gen)
 
 response = ul.request.urlopen(url)    
-file = open("HP" + ".pdf", 'wb')
+file = open("/tmp/HP.pdf", 'wb')
 file.write(response.read())
 file.close()
