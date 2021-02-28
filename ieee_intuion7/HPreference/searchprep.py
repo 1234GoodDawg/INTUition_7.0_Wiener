47aca03f366d5c819d085567ce69e30e6f75715d
Tangolin <65639845+Tangolin@users.noreply.github.com>
2021-02-28 08:28:00 +0800

    Update searchprep.py

--- ieee_intuion7/HPreference/searchprep.py
+++ ieee_intuion7/HPreference/searchprep.py
@@ -1,13 +1,20 @@
 import pandas as pd
 from search_pdf import find
+import os 
+
+
 
 def prep(filename):
+    mypath = '/tmp/'
+    if not os.path.isdir(mypath):
+        os.makedirs(mypath)
+        
     temp = pd.read_csv(filename)
     chemical_names = set(temp['Name'].to_list())
     i = 0
     
     for item in chemical_names:
-        if(find(item)):
+        if(find(item,i)):
             i +=1
             print(f"Found {i}/{len(chemical_names)} requirements"}
             continue
@@ -15,7 +22,7 @@ def prep(filename):
             wordlist = item.split(' ')
             new = [x for x in wordlist if x not in ['and','other','or','the','in']]
             for elem in new:
-                if(find(elem)):
+                if(find(elem,i)):
                     i +=1
                     print(f"Found {i}/{len(chemical_names)} requirements"}
                     break
